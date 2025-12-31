b0188eece8b6cad298d2a098cb633da22de83e2b
Bigyan Pradhan <pradhan.bigyan1717@gmail.com>
2025-12-31 11:40:27 +0545

    Solved anagram problem

---
+++ anagram.js
@@ -0,0 +1,16 @@
+var anagram = function (str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let charCount = new Array(26).fill(0);
+
+  for (let i = 0; i < str1.length; i++) {
+    charCount[str1.charCodeAt(i) - 97]++;
+    charCount[str2.charCodeAt(i) - 97]--;
+  }
+
+  return charCount.every((val) => val === 0);
+};
+
+console.log(anagram("car", "rac"));
